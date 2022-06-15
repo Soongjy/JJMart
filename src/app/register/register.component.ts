4aea49e4f836b84e2b7dd70848bd36b33d4c6d89
Soongjy <jiayangsoong@GMAIL.COM>
2022-06-15 12:55:28 +0800

    update sign up

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter} from '@angular/core';
 import { UserService } from '../services/user.service';
 import { User } from '../Users';
 
@@ -8,7 +8,14 @@ import { User } from '../Users';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
   users: User[] = [];
+  @Output() onAddUser: EventEmitter<User> =new EventEmitter();
+  name!: string;
+  username!: string;
+  email!: string;
+  password!: string;
+  repassword!: string;
 
   constructor(private userService: UserService) { }
 
@@ -16,4 +23,35 @@ export class RegisterComponent implements OnInit {
     this.userService.getTask().subscribe((users)=> (this.users = users))
   }
 
+  onSubmit() {
+    if(!this.name){
+      alert('Please fill in your name')
+    }else if(!this.email){
+      alert('Please fill in your email')
+    }else if(!this.password){
+      alert('Please fill in your password')
+    }else if(!this.repassword){
+      alert('Please fill in your password')
+    }else if(this.password !== this.repassword){
+      alert('Password and Repeat password is not the same')
+      this.password = '';
+      this.repassword = '';
+      return
+    }
+
+    const newUser = {
+      name: this.name,
+      username :this.username,
+      email: this.email,
+      password: this.password
+    };
+
+    this.onAddUser.emit(newUser); 
+
+    this.name = '';
+    this.username = '';
+    this.email = '';
+    this.password = '';
+    this.repassword = '';
+  }
 }
