d143c5d3ac83383b781db9f8d9d52e61f92cea5c
jessie <jessieloke18@gmail.com>
2022-07-01 09:19:51 +0800

    made product details 'add to cart' button work

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../Product';
+import { CartService } from '../services/cart.service';
 import { ProductService } from '../services/product.service';
 
 @Component({
@@ -12,7 +14,7 @@ export class ProductDetailsComponent implements OnInit {
 
   product !: Product;
 
-  constructor(private route: ActivatedRoute, private productService: ProductService) { }
+  constructor(private route: ActivatedRoute, private productService: ProductService, private cartService: CartService, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.getDetails();
@@ -23,5 +25,17 @@ export class ProductDetailsComponent implements OnInit {
     this.productService.getProduct(id)
       .subscribe(product => this.product = product);
   }
+
+  addToCart(product: Product){
+
+    this.cartService.addToCart(product);
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(this.product.name + " added to cart!", action, {
+      duration: 2000
+    });
+  }
+
 
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -24,7 +24,7 @@ export class ProductComponent implements OnInit {
   }
 
   addToCart(product: Product){
-   
+
     this.cartService.addToCart(product);
   }
 
