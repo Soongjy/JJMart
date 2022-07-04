1ba6004b0f3a7bcce4a4c322cbda9ce80d06c828
jessie <jessieloke18@gmail.com>
2022-07-04 11:34:37 +0800

    added snackbar and ngif else statement to ask user to login before adding item to cart

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -11,16 +11,34 @@ import { CartService } from '../services/cart.service';
 export class ProductComponent implements OnInit {
 
   @Input() product !: Product;
-  
+
+  userdetails: any;
+  name:string ='';
+
   constructor(private cartService: CartService, private _snackBar: MatSnackBar) { }
 
-  openSnackBar(message: string, action: string) {
+  openSnackBarAddCart(message: string, action: string) {
     this._snackBar.open(this.product.name + " added to cart!", action, {
       duration: 2000
     });
   }
 
+  openSnackBarSignIn(message: string, action: string) {
+    this._snackBar.open("Please sign in first!", action, {
+      duration: 2000
+    });
+  }
+
   ngOnInit(): void {
+
+    this.userdetails = JSON.parse(localStorage.getItem('userdetails')||"[]");
+  
+    if (this.userdetails == null) {
+      console.log("Please sign in first")
+    }
+    else{
+      console.log("user logged in")
+    }
   }
 
   addToCart(product: Product){
