6ddddb20fdf470dcbf770fd78d448d1d22ba2fff
Soongjy <jiayangsoong@GMAIL.COM>
2022-07-28 18:05:28 +0800

    footer update

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -20,7 +20,7 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
     this.currentYear = new Date().getFullYear();
 
-    this.admindetails = JSON.parse(localStorage.getItem('admindetails')||"[]");
+    this.admindetails = JSON.parse(sessionStorage.getItem('admindetails')!);
     this.companyService.getCompanyInfo(1).subscribe(company=>{
       this.companyLogo = company.companyLogo;
       this.companySlogan = company.companySlogan;
