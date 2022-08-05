5f8c36bf2b6116a67adc1d3f68fbe4c2d0d107ea
Soongjy <jiayangsoong@GMAIL.COM>
2022-08-05 12:24:07 +0800

    update

--- src/app/admin/my-line-chart/my-line-chart.component.ts
+++ src/app/admin/my-line-chart/my-line-chart.component.ts
@@ -13,45 +13,47 @@ import {default as Annotation} from 'chartjs-plugin-annotation';
 
 export class MyLineChartComponent implements OnInit{
   year = new Date().getFullYear()
+  data:number[]=[]
+  lineChartData!: ChartConfiguration['data']
 
   constructor(private orderService: OrderService) {
-    Chart.register(Annotation)
+    Chart.register(Annotation);
   }
 
   ngOnInit(): void {
     this.orderService.getOrders().subscribe(
       (orders)=>{
-          for(let array=0; array<12; array++){
-            var monthlyRevenue = 0;
-            for(let i=0; i<orders.length; i++){
-              var date:Date = new Date(orders[i].orderDate)
-              var orderMonth =date.getMonth();
-              if(array == orderMonth){
-                monthlyRevenue = monthlyRevenue + orders[i].totalPrice
-              }
+        for(let array=0; array<12; array++){
+          var monthlyRevenue = 0;
+          for(let i=0; i<orders.length; i++){
+            var date:Date = new Date(orders[i].orderDate)
+            var orderMonth =date.getMonth();
+            if(array == orderMonth){
+              monthlyRevenue = monthlyRevenue + orders[i].totalPrice
             }
-            this.lineChartData.datasets[0].data.push(monthlyRevenue);
           }
+          this.data.push(monthlyRevenue);
         }
+        this.lineChartData= {
+          datasets: [
+            {
+              data: this.data,
+              label: 'Monthly Revenue',
+              backgroundColor: 'rgba(0, 255, 0, 0.3)',
+              borderColor: 'green',
+              pointBackgroundColor: 'rgba(148,159,177,1)',
+              pointBorderColor: '#fff',
+              pointHoverBackgroundColor: '#fff',
+              pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+              fill: 'origin',
+            },
+          ],
+          labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October','November','December']
+        };
+      }
     );
   }
-
-  public lineChartData: ChartConfiguration['data'] = {
-    datasets: [
-      {
-        data: [],
-        label: 'Monthly Revenue',
-        backgroundColor: 'rgba(148,159,177,0.2)',
-        borderColor: 'rgba(148,159,177,1)',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
-        fill: 'origin',
-      },
-    ],
-    labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October','November','December' ]
-  };
+  // this.data[0],this.data[1],this.data[2],this.data[3],this.data[4],this.data[5],this.data[6],this.data[7],this.data[8],this.data[9],this.data[10],this.data[11],this.data[12]
   
 
   public lineChartOptions: ChartConfiguration['options'] = {
